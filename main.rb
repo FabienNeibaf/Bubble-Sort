b0db94bd817685032357e7b097c9e586c5da8fa2
Fabien RAKOTOMAMPIANDRA <fabienlintone@yahoo.fr>
2019-04-02 16:40:30 +0300

    Finish Bubble Sort method

---
+++ main.rb
@@ -0,0 +1,22 @@
+def buble_sort_by(arr)
+  res = arr.clone
+  com = -> (i) { res[i], res[i+1] = res[i+1], res[i] }
+  i = res.size - 1
+  until i == 0
+    j = 0
+    until j == i
+      if block_given?
+        com.call(j) if yield(res[j], res[j+1]) > 0
+      elsif res[j] > res[j+1]
+        com.call(j)
+      end
+      j += 1
+    end
+    i -= 1
+  end
+  return res
+end
+
+sorted = buble_sort_by(["hi","hello","hey"]) { |l, r| l.size - r.size }
+
+puts sorted
