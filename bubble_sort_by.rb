1468e6f4889604b103a519e37b7794a3c216567a
FabienNeibaf <fabienlintone@gmail.com>
2019-09-19 16:40:56 +0300

    Fix style error

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -2,19 +2,17 @@
 
 def bubble_sort_by(arr)
   res = arr.clone
-  commute = -> (i) { res[i], res[i+1] = res[i+1], res[i] }
   i = res.size - 1
-  until i == 0
-    j = 0
-    until j == i
-      if block_given?
-        commute.call(j) if yield(res[j], res[j+1]) > 0
-      elsif res[j] > res[j+1]
-        commute.call(j)
-      end
-      j += 1
-    end
-    i -= 1
+  until (i -= 1).zero?
+    j = -1
+    commute(res, j) while (j += 1) < i
   end
-  return res
+  res
+end
+
+def commute(arr, ind)
+  if (block_given? && yield(arr[ind], arr[ind + 1]).positive?) ||
+     arr[ind] > arr[ind + 1]
+    arr[ind], arr[ind + 1] = arr[ind + 1], arr[ind]
+  end
 end
--- main.rb
+++ main.rb
@@ -2,8 +2,8 @@
 
 require './bubble_sort_by'
 
-arr = ["hi","hello","hey"]
-arr2 = [5,6,8,9,7,4,5,1,2,10]
+arr = %w[hi hello hey]
+arr2 = [5, 6, 8, 9, 7, 4, 5, 1, 2, 10]
 
 sorted_s = bubble_sort_by(arr) { |l, r| l.size - r.size }
 sorted_n = bubble_sort_by(arr2)
